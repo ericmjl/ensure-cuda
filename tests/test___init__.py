bde1205ea62d29630d92f16d0303f9d0b7e6219d
Eric Ma <ericmajinglong@gmail.com>
2024-05-30 00:39:18 -0400

    feat(ensure_cuda): improve environment variable handling and package discovery
    - Add a print statement to notify when `LD_LIBRARY_PATH` is modified to include `$CONDA_PREFIX`.
    - Modify package discovery in `pyproject.toml` to search the entire project instead of just the `ensure_cuda` directory, removing the explicit script entry for better flexibility.
    - Update tests to initialize `LD_LIBRARY_PATH` as empty to better simulate a fresh environment and adjust conditions to ensure both `CONDA_PREFIX` and `LD_LIBRARY_PATH` are not None before asserting.
    
    This change enhances the usability of the ensure-cuda package by making its effects on the environment more transparent to the user and improving its compatibility with various project structures. Additionally, the test adjustments provide a more accurate representation of typical usage scenarios.

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -8,7 +8,14 @@ def test_ensure_cuda():
     The expected behavior is that ensure-cuda
     will add the conda-prefix's `/lib` directory to the LD_LIBRARY_PATH.
     """
+    os.environ["LD_LIBRARY_PATH"] = ""
+
     import ensure_cuda # noqa: F401, I001
 
-    if os.getenv("CONDA_PREFIX", None) is not None:
+
+    if (
+        os.getenv("CONDA_PREFIX", None) is not None
+        and
+        os.getenv("LD_LIBRARY_PATH", None) is not None
+    ):
         assert os.environ["CONDA_PREFIX"] in os.environ["LD_LIBRARY_PATH"]
